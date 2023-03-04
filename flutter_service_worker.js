b14327e6fc9de0713ce10f6f8726bffeb88f8e8f
AlejandroC <alexcastillo@cbluna.com>
2023-03-03 17:44:20 -0800

    V 1.5 push correction

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "029e95554b5884816e70b06a56b1db96",
-"index.html": "5c180525006a3f98a7c1a3c06c99f803",
-"/": "5c180525006a3f98a7c1a3c06c99f803",
+"index.html": "51835a3a572122bb7244196cc9066b6f",
+"/": "51835a3a572122bb7244196cc9066b6f",
 "main.dart.js": "b681f86369047c686f8e9429034b577b",
+"flutter.js": "1cfe996e845b3a8a33f57607e8b09ee4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -19,7 +20,11 @@ const RESOURCES = {
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080",
 "assets/assets/RiveAssets/GigOmeter.riv": "aeedc50b42e585b613e657c5f8b094dc",
-"assets/assets/LottieAssets/CircleCount.json": "f6bc429fc1e5074d11e2f4c2f53abc02"
+"assets/assets/LottieAssets/CircleCount.json": "f6bc429fc1e5074d11e2f4c2f53abc02",
+"canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
+"canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
+"canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
+"canvaskit/canvaskit.wasm": "3de12d898ec208a5f31362cc00f09b9e"
 };
 
 // The application shell files that are downloaded before a service worker can
